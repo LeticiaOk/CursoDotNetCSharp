942fb46441d694e91d74bf51594ad02422603c6e
Leticia Alves <130405282+LeticiaOk@users.noreply.github.com>
2024-09-18 10:51:20 -0300

    Delete ExemploPOO directory

--- ExemploPOO/aula/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using ExemploPOO.Interfaces;
-using ExemploPOO.Models;
-
-// O objeto da interface calculadora recebe a classe calculadora que implementa essa interface.
-ICalculadora calc = new Calculadora();
-System.Console.WriteLine(calc.Multiplicar(3, 9));
